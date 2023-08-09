889226162e58c511a3167794afe8514f259ced74
Deniz <132823753+DenizBaranAkyol@users.noreply.github.com>
2023-08-09 10:24:43 +0300

    add Iput

--- src/App.vue
+++ src/App.vue
@@ -78,6 +78,12 @@
           aria-describedby="inputGroup-sizing-sm">
       </div>
 
+      <div class="input-group input-group-sm mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-sm">Input</span>
+        <input v-model="Data.getID" type="text" class="form-control" aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-sm">
+      </div>
+
 
       Getirmek istediğin id = <input v-model="deger" type="text">
 
